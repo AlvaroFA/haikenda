a32cd20b3cc65c70f3f8e405f282471694903ca2
AlvaroFA <fragaarnaizalvaro@gmail.com>
2019-11-29 22:39:29 +0100

    added usar effect and get method to fecthing data from firebase

--- src/containers/TimetableForm/TimetableForm.js
+++ src/containers/TimetableForm/TimetableForm.js
@@ -1,97 +1,109 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import Border from '../../components/hoc/Border';
 import axios from '../../axios.app';
 
 const initialTimeTableForm = {
-    oneTime:{
-        elementType: 'input',
-        inputConfig: {
-            type: 'checkbox',
-            name: 'once',
-        },
-        value: '',
-        label:'Repetir una vez',
-        validation: {
-            required: true,
-        },
-        isValid: false,
-        
-    },
-    weekly:{
-        elementType: 'input',
-        inputConfig: {
-            type: 'checkbox',
-            name: 'weekly',
-        },
-        value: '',
-        label:'Semanalmente',
-        validation: {
-            required: true,
-        },
-        isValid: false,
-        
-    },
-    title: {
-        elementType: 'input',
-        inputConfig: {
-            type: 'text',
-            placeholder: 'Nombre del horario'
-        },
-        value: '',
-        label:'Titulo del horario',
-        validation: {
-            required: true,
-        },
-        isValid: false
-    },
-    startTime: {
-        elementType: 'input',
-        inputConfig: {
-            type: 'date',
-            placeholder: 'dd/mm/YYYY'
-        },
-        value: '',
-        label:'Fecha de inicio',
-        validation: {
-            required: true
-        },
-        isValid: false
-    },
-    endTime: {
-        elementType: 'input',
-        inputConfig: {
-            type: 'date',
-            placeholder: 'dd/mm/YYYY'
-        },
-        value: '',
-        label:'Fecha de fin',
-        validation: {
-            required: true
-        },
-        isValid: false
-    },
+    timeTableForm: {
+        oneTime: {
+            elementType: 'input',
+            inputConfig: {
+                type: 'checkbox',
+                name: 'once',
+            },
+            value: '',
+            label: 'Repetir una vez',
+            validation: {
+                required: true,
+            },
+            isValid: false,
 
+        },
+        weekly: {
+            elementType: 'input',
+            inputConfig: {
+                type: 'checkbox',
+                name: 'weekly',
+            },
+            value: '',
+            label: 'Semanalmente',
+            validation: {
+                required: true,
+            },
+            isValid: false,
+
+        },
+        title: {
+            elementType: 'input',
+            inputConfig: {
+                type: 'text',
+                placeholder: 'Nombre del horario'
+            },
+            value: '',
+            label: 'Titulo del horario',
+            validation: {
+                required: true,
+            },
+            isValid: false
+        },
+        startTime: {
+            elementType: 'input',
+            inputConfig: {
+                type: 'date',
+                placeholder: 'dd/mm/YYYY'
+            },
+            value: '',
+            label: 'Fecha de inicio',
+            validation: {
+                required: true
+            },
+            isValid: false
+        },
+        endTime: {
+            elementType: 'input',
+            inputConfig: {
+                type: 'date',
+                placeholder: 'dd/mm/YYYY'
+            },
+            value: '',
+            label: 'Fecha de fin',
+            validation: {
+                required: true
+            },
+            isValid: false
+        }
+    }
 
 }
 function TimeTableForm() {
 
     const [timeTableForm, setTimeTableform] = useState(initialTimeTableForm);
     const [creationCheck, setCreationCheck] = useState(false);
+    const isMounted = useRef(true);
 
-    const createTimetableHandler = () => {
 
-    };
-
-    const checkValidation= (value , rules) =>{
-           
-    
+    const checkValidation = (value, rules) => {
+        let itsOk;
+        if (rules.required) {
+            itsOk = value.trim() !== '' ? true : false;
+        }
     }
+
+    useEffect(() => {
+        axios.get('https://haikenda-6a939.firebaseio.com/timetable.json').then(response => {
+            if (isMounted.current == true) {
+                setTimeTableform({ ...timeTableForm, savedData: response.data });
+            }
+        })
+        return (() => {
+            isMounted.current = false;
+        });
+    });
 
     const inputChangeHandler = (evt, inputId) => {        // cloning the data 
         const newTimeTableForm = {
-            ...timeTableForm
+            ...timeTableForm.timeTableForm
         };
         //accessing to elements
         const updatedElement = { ...newTimeTableForm[inputId] };
@@ -102,7 +114,7 @@ function TimeTableForm() {
         // settings new values
         newTimeTableForm[inputId] = updatedElement;
         //overwritting the state
-        setTimeTableform(newTimeTableForm);
+        setTimeTableform({...timeTableForm, timeTableForm: newTimeTableForm});
 
     };
 
@@ -110,19 +122,19 @@ function TimeTableForm() {
         event.preventDefault();
         setCreationCheck(true);
         const timeTableData = {};
-        for( let timeTableElement in timeTableForm){
-            timeTableData[timeTableElement] = timeTableForm[timeTableElement].value;
+        for (let timeTableElement in timeTableForm.timeTableForm) {
+            timeTableData[timeTableElement] = timeTableForm.timeTableForm[timeTableElement].value;
         }
         axios.post('/timetable.json', timeTableData).then(response => console.log(response))
-        .catch(error=> console.log(error));    
+            .catch(error => console.log(error));
     };
 
 
     const formElementsArray = [];
-    for (let k in timeTableForm) {
+    for (let k in timeTableForm.timeTableForm) {
         formElementsArray.push({
             id: k,
-            config: timeTableForm[k]
+            config: timeTableForm.timeTableForm[k]
         });
     }
     let form = (
