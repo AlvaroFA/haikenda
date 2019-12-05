5a8ed9cb760dc79458a2f1263f7fe74c2ee5dbc9
Alvaro Fraga Arnaiz <fpd3132@xunta.es>
2019-12-06 00:48:11 +0100

    Enable showing validations errors in components

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -25,13 +25,15 @@ const Input = (props)=>{
             inputElement = <input {...props.inputConfig} value={props.value} onChange={props.changed} />;
     }
 
-    const errors = props.incorrectValues || [];
+    const errors = (Array.isArray(props.incorrectValues) && props.incorrectValues) || [];
     const label = props.inputConfig && props.inputConfig.hidden ? '' : <label>{props.label}</label> 
     return(
         <div>
         {label}
         {inputElement}
-
+        {
+            errors.map((item, key) => <span key={key} className="validation-error">{item}</span>)
+        }
         </div>
     );
 };
