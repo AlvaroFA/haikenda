d84534350f75857f2c2aae9a2f4422be0ebf156d
Alvaro <fpd3132@xunta.es>
2019-12-06 13:46:55 +0100

    Move timetableprovider from axios to firebase sdk

--- src/providers/TimetableProvider.js
+++ src/providers/TimetableProvider.js
@@ -1,23 +1,38 @@
-import axios from '../axios.app';
+import firebaseApp from '../components/firebase/Firebase';
 
-export function fetchTimetables(){
-    return axios.get('https://haikenda-6a939.firebaseio.com/timetable.json');
+const defaultEmptyTimetables = {};
+const defaultEmptyTimetable = {}
+
+const root="/timetable"
+
+export function fetchTimetables() {
+    return firebaseApp.database().ref(root).once('value').then((snapshot)=>{
+        if(snapshot)
+            return snapshot.val();
+        else 
+            return defaultEmptyTimetables;
+    });
 }
 
 export function createTimetable(timeTableData){
-    return axios.post('/timetable.json', timeTableData);
+    return firebaseApp.database().ref(root).push(timeTableData);
 }
 
 export function deleteTimetable(idTimetable) {
-    return axios.delete('https://haikenda-6a939.firebaseio.com/timetable/'+idTimetable+'.json');
+    return firebaseApp.database().ref(root+'/'+idTimetable).remove();
 }
 
 export function fetchOneTimetable(idTimetable) {
-    return axios.get('https://haikenda-6a939.firebaseio.com/timetable/'+idTimetable+'.json');
+    return firebaseApp.database().ref(root).child(idTimetable).once('value').then((snapshot)=>{
+        if(snapshot)
+            return snapshot.val() && snapshot.val();
+        else 
+            return defaultEmptyTimetable;
+    });
 }
 
 export function updateTimetable(timetableId, timeTableData){
-    return axios.put('/timetable/'+timetableId+'.json', timeTableData);
+    return firebaseApp.database().ref(root+"/"+timetableId).set(timeTableData);
 }
 
 export default {
