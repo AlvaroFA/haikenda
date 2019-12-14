7b85c376a74690c34048b035e142ee552e311170
AlvaroFA <fragaarnaizalvaro@gmail.com>
2019-12-14 19:23:58 +0100

    Autoformat

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import MenuBar from  '../Navigation/MenuBar/MenuBar';
+import MenuBar from '../Navigation/MenuBar/MenuBar';
 import './Layout.css';
 import NavigationItems from '../Navigation/NavigationItems/NavigationItems';
 
 
 //import Timetable from '../../containers/TimeTable';
 
-const Layout = ({children, isLoggedIn, isAdmin, user})=>(
+const Layout = ({ children, isLoggedIn, isAdmin, user }) => (
     <div className="Layout">
-        <MenuBar isLoggedIn={isLoggedIn} isAdmin={isAdmin} user={user}/>
+        <MenuBar isLoggedIn={isLoggedIn} isAdmin={isAdmin} user={user} />
         <nav>
-            <NavigationItems user={user} isLoggedIn={isLoggedIn} isAdmin={isAdmin}/>
+            <NavigationItems user={user} isLoggedIn={isLoggedIn} isAdmin={isAdmin} />
         </nav>
-
         <main className="LayoutMain">
             {children}
         </main>
